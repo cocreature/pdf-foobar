5c62ab4be0c364e4f045e0bda6cabdbef5f1cc60
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2017-09-04 17:43:43 +0200

    Use custom build of mupdf to only include mutool

--- default.nix
+++ default.nix
@@ -3,13 +3,14 @@ let
   ghcjs = pkgs.haskell.packages.ghcjsHEAD;
   ghc = pkgs.haskell.packages.ghc802;
   miso-ghcjs = ghcjs.callPackage ./miso-ghcjs.nix {};
+  mupdf = pkgs.callPackage ./mupdf.nix {};
 in
   rec {
     frontend = pkgs.haskell.lib.disableSharedExecutables (ghcjs.callPackage ./pdf-foobar-frontend.nix { miso = miso-ghcjs; });
     backend = pkgs.haskell.lib.disableSharedExecutables (ghc.callPackage ./pdf-foobar-backend.nix {});
     docker-image = pkgs.dockerTools.buildImage {
       name = "cocreature/pdf-foobar";
-      contents = [frontend backend pkgs.mupdf.bin];
+      contents = [frontend backend mupdf.bin];
       runAsRoot = ''
         mkdir -p /tmp
       '';
